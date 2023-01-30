aa0a5ff2d596d418e426bf262bca639e515dee52
NetlifyDevAcc <119401954+NetlifyDevAcc@users.noreply.github.com>
2023-01-30 09:23:59 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Find din plads!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Click type your name and click the button!
         </p>
       </main>
 
