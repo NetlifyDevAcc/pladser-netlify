15ed92e670a409f9dd2acbe385a1ffe34d033d9b
NetlifyDevAcc <119401954+NetlifyDevAcc@users.noreply.github.com>
2023-02-02 20:20:34 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,12 @@ function random() {
     var x = Math.sin(seed++) * 10000;
     return x - Math.floor(x);
 }
+Date.prototype.getWeek = function() {
+  var onejan = new Date(this.getFullYear(),0,1);
+  var today = new Date(this.getFullYear(),this.getMonth(),this.getDate());
+  var dayOfYear = ((today - onejan + 86400000)/86400000);
+  return Math.ceil(dayOfYear/7)
+};
 function giveNumber() {
     
   var date = new Date();
